7ad43a636e89d11de75de738556b426aff53a012
Camila Paredes <ceparedes@uc.cl>
2022-08-13 11:57:47 -0400

    .

--- Actividad 2.R
+++ Actividad 2.R
@@ -16,6 +16,16 @@ remote_driver$navigate("https://www.santander.cl/cotizador-web/")
 
 # pagina %>% html_element(xpath = '....') # Con rvest
 
+AÑO = '2022'
+MARCA    = 'JEEP'
+MODELO   = 'RENEGADE'
+RUT      = '24312997-4'
+NOMBRE   = 'Diego Muñoz'
+FechaN   = '10/01/1997'
+SEXO     = 'Masculino'
+EMAIL    = 'correo_generico@gmail.com'
+TELEFONO = runif(1,900000000,999999999) %>% round(0) %>% as.character()
+
 # Seleccionar marca del vehículo
 
 remote_driver$findElement(using = 'xpath',
@@ -34,3 +44,27 @@ remote_driver$findElement(using = 'xpath',
                           value = paste0('//select[@id = "marcas"]/option[@value = ',ID_MARCA,']'))$clickElement()
 
 # Seleccionar el modelo del vehículos
+LISTA_MODELOS <- remote_driver$findElement(using = 'id',
+                                          value = 'modelos')$selectTag()
+
+ID_MODELO <- tibble(Nombre_modelo = LISTA_MODELOS$text,
+                    value_modelo  = LISTA_MODELOS$value) %>% 
+  filter(Nombre_modelo == 'RENEGADE') %>% 
+  pull(value_modelo)
+
+
+remote_driver$findElement(using = 'xpath',
+                          value = paste0('//select[@id = "modelos"]/option[@value = ',ID_MODELO,']'))$clickElement()
+
+# Ingresar Año
+
+remote_driver$findElement(using = 'id',
+                          value = 'ano')$sendKeysToElement(list(AÑO))
+
+# Siguiente página
+remote_driver$findElement(using = 'id',
+                          value = 'siguiente1')$clickElement()
+
+# RUT
+
+
