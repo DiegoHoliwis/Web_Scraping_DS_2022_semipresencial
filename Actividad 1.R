7d11790556486d7f00a70aab52a9be1d7cfe4716
Camila Paredes <ceparedes@uc.cl>
2022-08-13 11:25:31 -0400

    .

--- Actividad 1.R
+++ Actividad 1.R
@@ -70,17 +70,120 @@ pagina %>%
       if_else(is.na(.),'Sin envio gratis',.)
   })
 
+# Cómo utilizar map, se debe llamar a la función, esta sin ejecutar, osea, sin () al final.
 # map_int(.f = sum)
 # map_int(.f = function(x){sum(x,na.rm = TRUE)})
 
-perrito <- function(x){
-  pagina %>% 
-    html_element(.,xpath = paste0('//ol[@class = "items_container"]/li[',x,']//span[@class = "promotion-item__shipping"]')) %>% 
-    html_text2(.) %>% 
-    if_else(is.na(.),'Sin envio gratis',.)}
+# perrito <- function(x){
+#   pagina %>% 
+#     html_element(.,xpath = paste0('//ol[@class = "items_container"]/li[',x,']//span[@class = "promotion-item__shipping"]')) %>% 
+#     html_text2(.) %>% 
+#     if_else(is.na(.),'Sin envio gratis',.)}
+# 
+# 1:length(Nombre) %>% 
+#   map_chr(.f = perrito)
 
-1:length(Nombre) %>% 
-  map_chr(.f = perrito)
+# Vendedor
+vendedor <- 1:length(Nombre) %>% 
+  map_chr(.f = function(x){
+    pagina %>% 
+      html_element(xpath = paste0('//ol[@class = "items_container"]/li[',x,']//span[@class = "promotion-item__seller"]')) %>% 
+      html_text2() %>% 
+      if_else(is.na(.),'Sin información',.)
+  })
+
+# URL del producto 
+URL <- pagina %>% 
+  html_elements(xpath = '//a[@class = "promotion-item__link-container"]') %>% 
+  html_attr('href')
+
+
+# Función que extrae toda la información
+
+scraping <- function(){
+  
+  pagina <- read_html('https://www.mercadolibre.cl/ofertas?promotion_type=deal_of_the_day&container_id=MLC779365-1&page=1')
+  
+  N_paginas <- pagina %>% 
+    html_element(xpath = '//ul[@class = "andes-pagination"]') %>% 
+    html_children() %>% 
+    html_text2() %>% 
+    as.numeric() %>% 
+    max(na.rm = TRUE) %>% 
+    suppressWarnings()
+  
+  1:N_paginas %>% 
+    map_dfr(.f = function(y){
+      
+      pagina <- read_html(paste0('https://www.mercadolibre.cl/ofertas?promotion_type=deal_of_the_day&container_id=MLC779365-1&page=',y))
+    
+      # Nombre
+      Nombre <- pagina %>% 
+        html_elements(xpath = '//p[@class = "promotion-item__title"]') %>% 
+        html_text2()
+      
+      # Precio antiguo
+      Precio_antiguo <- pagina %>% 
+        html_elements(xpath = '//span[@class = "promotion-item__oldprice"]') %>% 
+        html_text2() %>% 
+        str_remove_all('\\$') %>% 
+        str_trim() %>% 
+        str_remove_all('\\.') %>% 
+        as.numeric()
+      
+      # Precio oferta
+      Precio_oferta <- pagina %>% 
+        html_elements(xpath = '//span[@class = "promotion-item__price"]') %>% 
+        html_text2() %>% 
+        str_remove_all('\\$') %>% 
+        str_trim() %>% 
+        str_remove_all('\\.') %>% 
+        as.numeric()
+      
+      # Envió gratis
+    
+      Envio <- 1:length(Nombre) %>% 
+        map_chr(.f = function(x){
+          pagina %>% 
+            html_element(xpath = paste0('//ol[@class = "items_container"]/li[',x,']//span[@class = "promotion-item__shipping"]')) %>% 
+            html_text2() %>% 
+            if_else(is.na(.),'Sin envio gratis',.)
+        })
+      
+      # Vendedor
+      vendedor <- 1:length(Nombre) %>% 
+        map_chr(.f = function(x){
+          pagina %>% 
+            html_element(xpath = paste0('//ol[@class = "items_container"]/li[',x,']//span[@class = "promotion-item__seller"]')) %>% 
+            html_text2() %>% 
+            if_else(is.na(.),'Sin información',.)
+        })
+      
+      # URL del producto 
+      URL <- pagina %>% 
+        html_elements(xpath = '//a[@class = "promotion-item__link-container"]') %>% 
+        html_attr('href')
+      
+      
+      Sys.sleep(runif(1,1,1.5))
+      
+      tibble(Nombre,
+             Precio_antiguo,
+             Precio_oferta,
+             Envio,
+             vendedor,
+             URL)
+      
+    })
+  
+}
+
+datos_mercado_libre <- scraping()
+
+
+
+
+
 
 
 
