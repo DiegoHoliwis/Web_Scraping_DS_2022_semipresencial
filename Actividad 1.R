ae2efa2aa8cda4619a11c69c89282f32b033be80
Camila Paredes <ceparedes@uc.cl>
2022-08-13 10:36:31 -0400

    .

--- Actividad 1.R
+++ Actividad 1.R
@@ -1,2 +1,15 @@
 # Web scraping
 # Actividad 1
+
+library(tidyverse)
+library(rvest)
+
+
+pagina <- read_html('https://www.mercadolibre.cl/ofertas?promotion_type=deal_of_the_day&container_id=MLC779365-1&page=1')
+
+'class = "hola"'
+
+pagina %>% 
+  html_element(xpath = '//ul[@class = "andes-pagination"]') %>% 
+  html_children() %>% 
+  html_text2()
