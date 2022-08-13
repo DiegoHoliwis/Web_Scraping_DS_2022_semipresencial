5a38f1ef77c53598ca29bfae727a63fc076035d5
Camila Paredes <ceparedes@uc.cl>
2022-08-13 11:33:55 -0400

    .

--- Actividad 2.R
+++ Actividad 2.R
@@ -5,3 +5,11 @@ library(tidyverse)
 library(rvest)
 library(RSelenium)
 
+# Configuración inicial (siempre la misma)
+system("taskkill /im java.exe /f", intern=FALSE, ignore.stdout=FALSE) # Se finalizan los procesos Java
+
+driver <- RSelenium::rsDriver(browser = "chrome", 
+                              chromever = "104.0.5112.79")
+remote_driver <- driver[["client"]]
+
+
