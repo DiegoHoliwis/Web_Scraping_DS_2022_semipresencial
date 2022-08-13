d76e9c4da2b5c25e178e0a1c07ea96e4782e7e2d
Camila Paredes <ceparedes@uc.cl>
2022-08-13 10:42:44 -0400

    .

--- Actividad 1.R
+++ Actividad 1.R
@@ -3,7 +3,7 @@
 
 library(tidyverse)
 library(rvest)
-
+# options(warn = -1)
 
 pagina <- read_html('https://www.mercadolibre.cl/ofertas?promotion_type=deal_of_the_day&container_id=MLC779365-1&page=1')
 
@@ -12,4 +12,18 @@ pagina <- read_html('https://www.mercadolibre.cl/ofertas?promotion_type=deal_of_
 pagina %>% 
   html_element(xpath = '//ul[@class = "andes-pagination"]') %>% 
   html_children() %>% 
+  html_text2() %>% 
+  as.numeric() %>% 
+  max(na.rm = TRUE) %>% 
+  suppressWarnings()
+
+# Extraer titulo del producto
+pagina %>% 
+  html_element(xpath = '//p[@class = "promotion-item__title"]') %>% 
   html_text2()
+
+
+
+
+
+
