bff00e64d06e40ac34d3172fed93bce31781d02b
Camila Paredes <ceparedes@uc.cl>
2022-08-13 11:52:16 -0400

    .

--- Actividad 2.R
+++ Actividad 2.R
@@ -13,3 +13,24 @@ driver <- RSelenium::rsDriver(browser = "chrome",
 remote_driver <- driver[["client"]]
 
 remote_driver$navigate("https://www.santander.cl/cotizador-web/")
+
+# pagina %>% html_element(xpath = '....') # Con rvest
+
+# Seleccionar marca del vehículo
+
+remote_driver$findElement(using = 'xpath',
+                          value = '//select[@id = "marcas"]')
+
+LISTA_MARCAS <- remote_driver$findElement(using = 'id',
+                          value = 'marcas')$selectTag()
+
+ID_MARCA <- tibble(Nombre_marca = LISTA_MARCAS$text,
+       value_marca  = LISTA_MARCAS$value) %>% 
+  filter(Nombre_marca == 'JEEP') %>% 
+  pull(value_marca)
+
+
+remote_driver$findElement(using = 'xpath',
+                          value = paste0('//select[@id = "marcas"]/option[@value = ',ID_MARCA,']'))$clickElement()
+
+# Seleccionar el modelo del vehículos
